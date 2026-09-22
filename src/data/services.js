6ffceed2aac98272a907bc5b8aae6f1b4467ece7
powder-rose <glittervodka@yandex.ru>
2026-09-22 08:46:12 +0000

    Track frontend data source modules

---
+++ src/data/services.js
@@ -0,0 +1,38 @@
+export const services = [
+  {
+    id: 'atz-docs',
+    eyebrow: 'Документы',
+    title: 'Документация по антитеррористической защищённости',
+    description:
+      'Разработка приказов, инструкций, положений, журналов и других документов для организации антитеррористической защищённости объекта.',
+    href: 'https://boykovgroup.ru/dokumentaciya-po-antiterroristicheskoy-bezopasnosti',
+  },
+  {
+    id: 'dsp-docs',
+    eyebrow: 'Для служебного пользования',
+    title: 'Документы ДСП',
+    description:
+      'Подготовка документов с пометкой «Для служебного пользования», а также материалов по их учёту, хранению и использованию.',
+    href: 'https://boykovgroup.ru/dokumenty-dsp',
+  },
+  {
+    id: 'atz-training',
+    eyebrow: 'Организация обучения',
+    title: 'Организация обучения по антитеррористической защищённости',
+    description:
+      'Подготовка руководителей и ответственных работников по вопросам антитеррористической защищённости и действиям при возникновении угроз.',
+    href: 'https://boykovgroup.ru/obuchenie-po-antiterroristicheskoy-zashchishchennosti',
+  },
+  {
+    id: 'passport-actualization',
+    eyebrow: 'Актуализация',
+    title: 'Актуализация паспорта безопасности',
+    description:
+      'Проверим действующий паспорт, основания для внесения изменений или замены документа и подготовим актуальную редакцию.',
+    href:
+      '/aktualizaciya-pasporta-bezopasnosti-obekta/',
+    linkLabel:
+      'Подробнее об актуализации',
+  },
+
+];
